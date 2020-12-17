b99596065fc5980bb478e9f463ca31e6d8da2152
miguelAgudelo <miguelangelagudelolopera@gmail.com>
2020-12-17 08:57:35 -0500

    Index href
    Adding index href from citas

--- citas.php
+++ citas.php
@@ -17,7 +17,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#"><strong>Clínica del Dolor</strong></a>
+            <a class="navbar-brand" href="index.php"><strong>Clínica del Dolor</strong></a>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
