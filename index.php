023b10ce038cd5f2ede8d4d9017f576f4d711211
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-12-17 00:19:39 -0500

    Revert "Ajustes de imagen"
    This reverts commit 39e6f5b7868aec12e98ca0586b832a2294dd563e.

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clínica del Dolor</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
 
@@ -18,90 +17,36 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link btn btn-outline-light mr-3 mt-1" href="index.php"><strong>Inicio</strong></a>
+                        <a class="nav-link btn btn-outline-info mr-3 mt-1" href="index.php"><strong>Inicio</strong></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-outline-light mr-3 mt-1" href="citas.php">Citas</a>
+                    <li class="nav-item active">
+                        <a class="nav-link btn btn-outline-info mr-3 mt-1" href="citas.php">Citas</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-outline-light mr-3 mt-1" href="#">Gestión Interna</a>
+                    <li class="nav-item active">
+                        <a class="nav-link btn btn-outline-info mr-3 mt-1" href="#">Gestión Interna</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-outline-light mt-1" href="#">Contacto</a>
+                    <li class="nav-item active">
+                        <a class="nav-link btn btn-outline-info mt-1" href="#">Contacto</a>
                     </li>
                 </ul>
             </div>   
         </nav>       
     </header>
 
-    <main class="fondo">
+    <main>
         <div class="container">
             <div class="row justify-content-center mt-5">
-                <div class="col-8">
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="img/medicos2.jpg" class="d-block w-100" alt="medicos">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5>Uso de tapabocas</h5>
-                                <p>Cúbrase la boca y la nariz con una mascarilla cuando está con otras personas.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="img/covid.jpg" class="d-block w-100" alt="covid">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5 class="text-dark">Lavado demanos</h5>
-                                <p class="text-dark">Por lo menos cada tres horas con agua y jabón durante 20 segundos.</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="img/controla_tu_salud.jpg" class="d-block w-100" alt="salud">
-                            <div class="carousel-caption d-none d-md-block">
-                                <h5 class="text-dark">Monitoree su salud a diario</h5>
-                                <p class="text-dark">1. Esté atento a los síntoma.</p>
-                                <p class="text-dark">2. Controle su temperatura.</p>
-                            </div>
-                        </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                        </a>
-                    </div>
+                <div class="col-md-12 mt-5">
+                    <h1 class="text-center text-primary mt-5">EN CONSTRUCCION</h1>
                 </div>
-                            
             </div>
+                      
         </div>
    
     </main>
 
-    <footer class="bg-primary text-light">
-        <div class="container">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-12 col-md-3 text-right mt-2">
-                    <address>
-                        Clínica del Dolor <br>
-                        Todos los derechos reservados <br>
-                        Medellín, 2020
-                    </address>                  
-                </div>
-                <div class="col-12 col-md-3 text-md-left text-right mb-2">
-                    <a href="https://www.instagram.com/lamachucacervezaartesanal/" class="mr-2" target="_blank">
-                    <img src="https://img.icons8.com/fluent/36/000000/instagram-new.png"/>
-                    </a>
-                    <a href="https://www.facebook.com/LaMachucaCerveza/" class="mr-2" target="_blank">
-                    <img src="https://img.icons8.com/ios/32/000000/facebook--v1.png"/>
-                    </a>
-                    <a href="https://twitter.com/fabianmachuca" target="_blank">
-                    <img src="https://img.icons8.com/ios-filled/32/000000/twitter-squared.png"/>
-                    </a>
-                </div>
-            </div>
-        </div>
+    <footer>
+
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
